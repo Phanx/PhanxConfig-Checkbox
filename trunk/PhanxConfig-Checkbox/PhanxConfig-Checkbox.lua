f6a56b37910ea19bdecc585cc9f7730733eec970
Phanx-28751 <Phanx-28751@5d13539c-25a5-4bc8-b52a-5474e3ee8778>
2014-02-07 22:29:53 +0000

    - Many consistent, all sides!

--- trunk/PhanxConfig-Checkbox/PhanxConfig-Checkbox.lua
+++ trunk/PhanxConfig-Checkbox/PhanxConfig-Checkbox.lua
@@ -14,23 +14,28 @@ local MINOR_VERSION = tonumber(strmatch("$Revision$", "%d+"))
 local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Checkbox", MINOR_VERSION)
 if not lib then return end
 
+------------------------------------------------------------------------
+
 local scripts = {}
 
 function scripts:OnClick()
 	local checked = self:GetChecked() == 1
 	PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainMenuOptionCheckBoxOff")
 	self:GetScript("OnLeave")(self)
-	local handler = self.ApplyValue or self.OnValueChanged or self.OnClick -- OnClick is deprecated
+	local handler = self.func or self.ApplyValue or self.OnValueChanged or self.OnClick -- OnClick is deprecated
 	if handler then
 		handler(self, checked)
 	end
 end
 
-function scripts:OnDisable()
-	if self.disabled then return end
-	local r, g, b = self.label:GetTextColor()
-	self.label:SetTextColor(r / 2, g / 2, b / 2)
-	self.disabled = true
+function scripts:OnEnter()
+	if self.tooltipText then
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+		GameTooltip:SetText(self.tooltipText, nil, nil, nil, nil, true)
+	end
+end
+function scripts:OnLeave()
+	GameTooltip:Hide()
 end
 
 function scripts:OnEnable()
@@ -39,36 +44,44 @@ function scripts:OnEnable()
 	self.label:SetTextColor(r * 2, g * 2, b * 2)
 	self.disabled = nil
 end
-
-function scripts:OnEnter()
-	if self.desc then
-		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-		GameTooltip:SetText(self.desc, nil, nil, nil, nil, true)
-	end
+function scripts:OnDisable()
+	if self.disabled then return end
+	local r, g, b = self.label:GetTextColor()
+	self.label:SetTextColor(r / 2, g / 2, b / 2)
+	self.disabled = true
 end
 
-function scripts:OnLeave()
-	GameTooltip:Hide()
-end
+------------------------------------------------------------------------
 
 local methods = {}
 
 function methods:GetValue()
 	return self:GetChecked() == 1
 end
-
 function methods:SetValue(value, auto)
 	self:SetChecked(value)
 end
 
-function lib:New(parent, text, desc)
+function methods:GetTooltipText()
+	return self.tooltipText
+end
+function methods:SetTooltipText(text)
+	self.tooltipText = text
+end
+
+function methods:SetFunction(func)
+	self.func = func
+end
+
+------------------------------------------------------------------------
+
+function lib:New(parent, text, tooltipText)
 	assert(type(parent) == "table" and type(rawget(parent, 0) == "userdata"), "PhanxConfig-Checkbox: parent must be a frame")
 	if type(name) ~= "string" then name = nil end
-	if type(desc) ~= "string" then desc = nil end
+	if type(tooltipText) ~= "string" then tooltipText = nil end
 
 	local check = CreateFrame("CheckButton", nil, parent)
 	check:SetSize(26, 26)
-	check:SetMotionScriptsWhileDisabled(true)
 
 	check:SetNormalTexture("Interface\\Buttons\\UI-CheckBox-Up")
 	check:SetPushedTexture("Interface\\Buttons\\UI-CheckBox-Down")
@@ -81,16 +94,16 @@ function lib:New(parent, text, desc)
 	label:SetText(text)
 	check.label = label
 
-	check:SetHitRectInsets(0, -1 * min(186, max(label:GetStringWidth(), 100)), 0, 0)
-
-	check.desc = desc
-
-	for name, func in pairs(methods) do
-		check[name] = func
-	end
+	check:SetMotionScriptsWhileDisabled(true)
 	for name, func in pairs(scripts) do
 		check:SetScript(name, func)
 	end
+	for name, func in pairs(methods) do
+		check[name] = func
+	end
+
+	check.tooltipText = tooltipText
+	check:SetHitRectInsets(0, -1 * min(186, max(label:GetStringWidth(), 100)), 0, 0)
 
 	return check
 end
